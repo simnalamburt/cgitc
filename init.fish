65d458655037408c41ad158db518c2bd0ce70eeb
Hyeon Kim <simnalamburt@gmail.com>
2016-01-02 18:04:01 +0900

    Basic aliases

--- init.fish
+++ init.fish
@@ -1,3 +1,22 @@
-function init -a path --on-event init_cgitc
-end
+#
+# Aliases
+#
 
+alias g='git'
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gapa='git add --patch'
+
+alias gc='git commit -v'
+alias gc!='git commit -v --amend'
+alias gca='git commit -v -a'
+
+alias gd='git diff'
+alias gdca='git diff --cached'
+
+alias gst='git status'
+
+alias glog='git log --oneline --decorate --color --graph'
+
+alias grh='git reset HEAD'
