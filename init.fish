9f9ce93f7cb6b6b2fc9c4a35dea624cc594256f8
Hyeon Kim <simnalamburt@gmail.com>
2016-01-20 21:08:08 +0900

    Use abbr instead of alias, initialize abbr only once
    Reference:
      https://github.com/fish-shell/fish-shell/issues/1976#issuecomment-168698602

--- init.fish
+++ init.fish
@@ -1,3 +1,138 @@
+if not set -q cgitc_initialized
+  set -U cgitc_initialized
+  printf 'Intializing \e[33mcgitc\e[0m ... '
+
+  # (sorted alphabetically)
+  abbr g 'git'
+
+  abbr ga 'git add'
+  abbr gaa 'git add --all'
+  abbr gapa 'git add --patch'
+
+  abbr gb 'git branch'
+  abbr gba 'git branch -a'
+  abbr gbl 'git blame -b -w'
+  abbr gbnm 'git branch --no-merged'
+  abbr gbr 'git branch --remote'
+  abbr gbs 'git bisect'
+  abbr gbsb 'git bisect bad'
+  abbr gbsg 'git bisect good'
+  abbr gbsr 'git bisect reset'
+  abbr gbss 'git bisect start'
+
+  abbr gc 'git commit -v'
+  abbr gc! 'git commit -v --amend'
+  abbr gca 'git commit -v -a'
+  abbr gca! 'git commit -v -a --amend'
+  abbr gcan! 'git commit -v -a -s --no-edit --amend'
+  abbr gcam 'git commit -a -m'
+  abbr gcb 'git checkout -b'
+  abbr gcf 'git config --list'
+  abbr gcl 'git clone --recursive'
+  abbr gclean 'git clean -fd'
+  abbr gcm 'git checkout master'
+  abbr gcmsg 'git commit -m'
+  abbr gco 'git checkout'
+  abbr gcount 'git shortlog -sn'
+  abbr gcp 'git cherry-pick'
+  abbr gcs 'git commit -S'
+
+  abbr gd 'git diff'
+  abbr gdca 'git diff --cached'
+  abbr gdct 'git describe --tags `git rev-list --tags --max-count=1`'
+  abbr gdt 'git diff-tree --no-commit-id --name-only -r'
+  abbr gdw 'git diff --word-diff'
+
+  abbr gf 'git fetch'
+  abbr gfa 'git fetch --all --prune'
+  abbr gfo 'git fetch origin'
+
+  abbr gg 'git gui citool'
+  abbr gga 'git gui citool --amend'
+
+  abbr ggpur 'ggu'
+
+  abbr gignore 'git update-index --assume-unchanged'
+  abbr gignored 'git ls-files -v | grep "^[[:lower:]]"'
+
+  abbr gk '\gitk --all --branches'
+
+  abbr gl 'git pull'
+  abbr glg 'git log --stat --color'
+  abbr glgp 'git log --stat --color -p'
+  abbr glgg 'git log --graph --color'
+  abbr glgga 'git log --graph --decorate --all'
+  abbr glgm 'git log --graph --max-count=10'
+  abbr glo 'git log --oneline --decorate --color'
+  abbr glol "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+  abbr glola "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+  abbr glog 'git log --oneline --decorate --color --graph'
+  abbr glp "_git_log_prettily"
+
+  abbr gm 'git merge'
+  abbr gmom 'git merge origin/master'
+  abbr gmt 'git mergetool --no-prompt'
+  abbr gmtvim 'git mergetool --no-prompt --tool=vimdiff'
+  abbr gmum 'git merge upstream/master'
+
+  abbr gp 'git push'
+  abbr gpd 'git push --dry-run'
+
+  abbr gpu 'git push upstream'
+  abbr gpv 'git push -v'
+
+  abbr gr 'git remote'
+  abbr gra 'git remote add'
+  abbr grb 'git rebase'
+  abbr grba 'git rebase --abort'
+  abbr grbc 'git rebase --continue'
+  abbr grbi 'git rebase -i'
+  abbr grbm 'git rebase master'
+  abbr grbs 'git rebase --skip'
+  abbr grh 'git reset HEAD'
+  abbr grhh 'git reset HEAD --hard'
+  abbr grmv 'git remote rename'
+  abbr grrm 'git remote remove'
+  abbr grset 'git remote set-url'
+
+  abbr gru 'git reset --'
+  abbr grup 'git remote update'
+  abbr grv 'git remote -v'
+
+  abbr gsb 'git status -sb'
+  abbr gsd 'git svn dcommit'
+  abbr gsi 'git submodule init'
+  abbr gsps 'git show --pretty=short --show-signature'
+  abbr gsr 'git svn rebase'
+  abbr gss 'git status -s'
+  abbr gst 'git status'
+  abbr gsta 'git stash'
+  abbr gstaa 'git stash apply'
+  abbr gstd 'git stash drop'
+  abbr gstl 'git stash list'
+  abbr gstp 'git stash pop'
+  abbr gsts 'git stash show --text'
+  abbr gsu 'git submodule update'
+
+  abbr gts 'git tag -s'
+  abbr gtv 'git tag | sort -V'
+
+  abbr gunignore 'git update-index --no-assume-unchanged'
+
+  abbr gup 'git pull --rebase'
+  abbr gupv 'git pull --rebase -v'
+  abbr glum 'git pull upstream master'
+
+  abbr gvt 'git verify-tag'
+
+  abbr gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
+
+  echo 'Done'
+end
+
+
+# TODO: Port aliases below
+
 # Query/use custom command for `git`.
 # zstyle -s ":vcs_info:git:*:-all-" "command" _omz_git_git_cmd
 # : ${_omz_git_git_cmd:=git}
@@ -33,64 +168,14 @@
 #   fi
 # }
 
-#
-# Aliases
-# (sorted alphabetically)
-#
 
-alias g='git'
-
-alias ga='git add'
-alias gaa='git add --all'
-alias gapa='git add --patch'
-
-alias gb='git branch'
-alias gba='git branch -a'
 # alias gbda='git branch --merged | command grep -vE "^(\*|\s*master\s*$)" | command xargs -n 1 git branch -d'
-alias gbl='git blame -b -w'
-alias gbnm='git branch --no-merged'
-alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
-
-alias gc='git commit -v'
-alias gc!='git commit -v --amend'
-alias gca='git commit -v -a'
-alias gca!='git commit -v -a --amend'
-alias gcan!='git commit -v -a -s --no-edit --amend'
-alias gcam='git commit -a -m'
-alias gcb='git checkout -b'
-alias gcf='git config --list'
-alias gcl='git clone --recursive'
-alias gclean='git clean -fd'
 # alias gpristine='git reset --hard && git clean -dfx'
-alias gcm='git checkout master'
-alias gcmsg='git commit -m'
-alias gco='git checkout'
-alias gcount='git shortlog -sn'
 # compdef gcount=git
-alias gcp='git cherry-pick'
-alias gcs='git commit -S'
-
-alias gd='git diff'
-alias gdca='git diff --cached'
-alias gdct='git describe --tags `git rev-list --tags --max-count=1`'
-alias gdt='git diff-tree --no-commit-id --name-only -r'
 # gdv() { git diff -w "$@" | view - }
 # compdef _git gdv=git-diff
-alias gdw='git diff --word-diff'
-
-alias gf='git fetch'
-alias gfa='git fetch --all --prune'
 # function gfg() { git ls-files | grep $@ }
 # compdef gfg=grep
-alias gfo='git fetch origin'
-
-alias gg='git gui citool'
-alias gga='git gui citool --amend'
 # ggf() {
 # [[ "$#" != 1 ]] && local b="$(git_current_branch)"
 # git push --force origin "${b:=$1}"
@@ -132,88 +217,22 @@ alias gga='git gui citool --amend'
 # git pull --rebase origin "${b:=$1}"
 # }
 # compdef _git ggu=git-checkout
-alias ggpur='ggu'
 # compdef _git ggpur=git-checkout
 
-alias gignore='git update-index --assume-unchanged'
-alias gignored='git ls-files -v | grep "^[[:lower:]]"'
 # alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
 # compdef git-svn-dcommit-push=git
 
-alias gk='\gitk --all --branches'
 # compdef _git gk='gitk'
 # alias gke='\gitk --all $(git log -g --pretty=format:%h)'
 # compdef _git gke='gitk'
 
-alias gl='git pull'
-alias glg='git log --stat --color'
-alias glgp='git log --stat --color -p'
-alias glgg='git log --graph --color'
-alias glgga='git log --graph --decorate --all'
-alias glgm='git log --graph --max-count=10'
-alias glo='git log --oneline --decorate --color'
-alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias glola="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
-alias glog='git log --oneline --decorate --color --graph'
-alias glp="_git_log_prettily"
 # compdef _git glp=git-log
 
-alias gm='git merge'
-alias gmom='git merge origin/master'
-alias gmt='git mergetool --no-prompt'
-alias gmtvim='git mergetool --no-prompt --tool=vimdiff'
-alias gmum='git merge upstream/master'
-
-alias gp='git push'
-alias gpd='git push --dry-run'
 # alias gpoat='git push origin --all && git push origin --tags'
 # compdef _git gpoat=git-push
-alias gpu='git push upstream'
-alias gpv='git push -v'
 
-alias gr='git remote'
-alias gra='git remote add'
-alias grb='git rebase'
-alias grba='git rebase --abort'
-alias grbc='git rebase --continue'
-alias grbi='git rebase -i'
-alias grbm='git rebase master'
-alias grbs='git rebase --skip'
-alias grh='git reset HEAD'
-alias grhh='git reset HEAD --hard'
-alias grmv='git remote rename'
-alias grrm='git remote remove'
-alias grset='git remote set-url'
 # alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
-alias gru='git reset --'
-alias grup='git remote update'
-alias grv='git remote -v'
 
-alias gsb='git status -sb'
-alias gsd='git svn dcommit'
-alias gsi='git submodule init'
-alias gsps='git show --pretty=short --show-signature'
-alias gsr='git svn rebase'
-alias gss='git status -s'
-alias gst='git status'
-alias gsta='git stash'
-alias gstaa='git stash apply'
-alias gstd='git stash drop'
-alias gstl='git stash list'
-alias gstp='git stash pop'
-alias gsts='git stash show --text'
-alias gsu='git submodule update'
-
-alias gts='git tag -s'
-alias gtv='git tag | sort -V'
-
-alias gunignore='git update-index --no-assume-unchanged'
 # alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
-alias gup='git pull --rebase'
-alias gupv='git pull --rebase -v'
-alias glum='git pull upstream master'
 
-alias gvt='git verify-tag'
-
-alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 # alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
