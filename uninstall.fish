89f8d4a0274c74a4ef297fb0c31ea4efaadc5301
Hyeon Kim <simnalamburt@gmail.com>
2016-01-21 20:53:12 +0900

    Implement uninstall script
    Successfully implemented uninstall script, but couldn't remove repeated
    codes
    
    Closes #5

--- uninstall.fish
+++ uninstall.fish
@@ -1,3 +1,17 @@
 function uninstall --on-event uninstall_cgitc
-  echo '(TODO) cgitc uninstall script not implemented yet!'
+  for line in (cat (dirname (status -f))/abbreviations)
+    # 1.  Strip out comments
+    # 2.  Squeeze repeating spaces
+    # 3.  Strip trailing whitespaces
+    set line (echo "$line" | cut -d '#' -f 1 | tr -s ' ' | sed 's/\s*$//')
+
+    # Skip empty lines
+    if not [ "$line" ]; continue; end
+
+    # Parse lines
+    set key (echo "$line" | cut -d ' ' -f 1)
+
+    abbr --erase "$key"
+  end
+  set --erase -U cgitc_initialized
 end
