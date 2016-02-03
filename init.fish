b1b1f4d44d6d7bb32c1275a2b0d564d4f6b4a03b
kinoru <xkinoru@gmail.com>
2016-02-03 17:21:21 +0900

    Fix the "git statu" bug (sed portability)
    [The `\s` is a GNU sed extension][sed] and is therefore not portable.
    On some systems (including OS X) `\s` is interpreted as a character `s`
    and therefore commands that end with `s` (such as `git status` or `git
    status -s`) will get the last letter stripped. For example, `gst`
    becomes `git statu`.
    
    The issue can easily be avoided with the more portable `[:space:]`
    character class that is part of the POSIX standard.
    
    [sed]: http://stackoverflow.com/a/10422020/2079797

--- init.fish
+++ init.fish
@@ -9,7 +9,7 @@ function init -a path --on-event init_cgitc
     # 1.  Strip out comments
     # 2.  Squeeze repeating spaces
     # 3.  Strip trailing whitespaces
-    set line (echo "$line" | cut -d '#' -f 1 | tr -s ' ' | sed 's/\s*$//')
+    set line (echo "$line" | cut -d '#' -f 1 | tr -s ' ' | sed 's/[[:space:]]*$//')
 
     # Skip empty lines
     if not [ "$line" ]; continue; end
