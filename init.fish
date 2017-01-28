b406fa1c67e1c2c2adb21bfa33fde4e32a160156
Hyeon Kim <simnalamburt@gmail.com>
2017-01-29 03:44:40 +0900

    fish: Support fish without abbr command
    Closes #20

--- init.fish
+++ init.fish
@@ -1,3 +1,23 @@
+# Support fish < 2.2
+switch "$FISH_VERSION"; case 2.1.2 2.1.1 2.1.0 2.0.0
+  for line in (cat (dirname (status -f))/abbreviations)
+    # 1.  Strip out comments
+    # 2.  Squeeze repeating spaces
+    # 3.  Strip trailing whitespaces
+    set line (echo "$line" | cut -d '#' -f 1 | tr -s ' ' | sed 's/[[:space:]]*$//')
+
+    # Skip empty lines
+    if not [ "$line" ]; continue; end
+
+    # Parse lines
+    set key   (echo "$line" | cut -d ' ' -f 1)
+    set value (echo "$line" | cut -d ' ' -f 2-)
+
+    alias $key $value
+  end
+  exit
+end
+
 # Skip if cgitc_revision is up to date
 set -l current_revision (git -C (dirname (status -f)) rev-parse HEAD)
 if [ "$cgitc_revision" != "$current_revision" ]
